7029583ea56b29930915a0c653f2b376c9f8f2e6
Aboud <aboudsabry19@gmail.com>
2023-04-11 13:00:04 +0300

    my first code push

---
+++ learn1.java
@@ -0,0 +1,9 @@
+/**
+ * learn1
+ */
+public class learn1 {
+public static void main(String[] args) {
+    System.out.println("Hello world");
+    System.out.println("So this is my first code for java");
+}
+}
