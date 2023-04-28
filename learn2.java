7f6265b0d293394d1637a5621f284be799fdf53d
Aboud <aboudsabry19@gmail.com>
2023-04-28 19:39:21 +0300

    just pushin p

---
+++ learn2.java
@@ -0,0 +1,12 @@
+/**
+ * learn2
+ */
+public class learn2 {
+
+    public static void main(String[] args) {
+        int num = 5;
+        int num2 = 6;
+        int num3 = num+num2;
+        System.out.println(num3);
+    }
+}
