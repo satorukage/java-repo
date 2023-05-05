3e453cb0744fd26eee828a780bf76105229e5698
Aboud <aboudsabry19@gmail.com>
2023-05-05 16:55:26 +0300

    Assignment 2

--- App.java
+++ App.java
@@ -1,74 +1,81 @@
 public class App {
+
     public static void main(String[] args) {
         System.out.println(section1());
         System.out.println(section2());
         System.out.println(section3());
-        //System.out.println(section4());
+        System.out.println(section4());
     }
 
     public static String section1() {
-        // Write section 1 code here
         int num = 42;
         float average = (float) 95.5;
-        char letter = 'A';
         boolean isTrue = true;
-
-        return "num: " + num + "\n" +
-             "average: " + average + "\n" +
-            "letter: " + letter + "\n" +
-              "isTrue: " + isTrue + "\n";
+        char letter = 'A';
+        return "num: "+num+"\n"+"average: "+average+"\n"+"letter: "+letter+"\n"+"isTrue: "+isTrue+"\n";
 
         // Hint: How to return the values as a string
-//        return "num: " + num + "\n" +
-//                "average: " + average + "\n" +
-//                "letter: " + letter + "\n" +
-//                "isTrue: " + isTrue + "\n";
+        // return "num: " + num + "\n" + "average: " + average + "\n" +"letter: " + letter + "\n" +"isTrue: " + isTrue + "\n";
     }
-
     public static String section2() {
-        // Write section 2 code here
         int num = 42;
         float average = (float) 95.5;
         boolean isTrue = true;
+        boolean isFalse = false;
 
-        int sum = num + 25;
-        float product = average * 2;
-        int modulus = num % 10;
-        boolean logic = isTrue && false;
-
-        return "sum: " + sum + "\n" +
-                "product: " + product + "\n" +
-                "modulus: " + modulus + "\n" +
-                "logicalAnd: " + logic + "\n" ;
+        int sum = num+25;
+        float product = average*2;
+        int modulus= num%10;
+        boolean logicalAnd = isTrue && isFalse;
+        return "sum: "+sum+"\n"+"product: "+product+"\n"+"modulus: "+modulus+"\n"+"logicalAnd: "+logicalAnd+"\n";
     }
-
-   /*  public static String section3() {
-        // Write section 3 code here
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < 10; i++) {
-            int square = i*i;
-            sb.append(square).append("");           
+    public static String section3() {
+        String result1 = "";
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            result1 = result1 + ("Square of " + i + ": " + square + "\n");
         }
-        String result = sb.toString();
 
         int num = 42;
+        String result2 = "";
         while (num < 100) {
-            sb.append(num).append("");
             num *= 2;
+            result2 = result2 + "num: " + num + " ";
         }
-
-        String result2 = sb.toString();
-
-        return result;
-       
-    }*/
-
-    public static String section4() {
-        // Write section 4 code here
-
+        // Combine the two results
+        return result1 + "\n" + result2 + "\n";
     }
 
-    
+    public static String section4() {
+        int num = 42;
+        char letter = 'A';
+        String result = "";
+
+        if (num % 2 == 0) {
+            result += "Even" +"\n";
+        } else {
+            result += "Odd" +"\n";
+        }
+        switch (letter) {
+            case 'A':
+                result += "Excellent" +"\n";
+                break;
+            case 'B':
+                result += "Good" +"\n";
+                break;
+            case 'C':
+                result += "Average" +"\n";
+                break;
+            case 'D':
+                result += "Poor" +"\n";
+                break;
+            case 'F':
+                result += "Fail" +"\n";
+                break;
+            default:
+                result += "Invalid grade" +"\n";
+                break;
+        }
+        return result;
+    }
 }
